67c2d6555728d362183f6c8df2f41038db070c9a
calvin <calvin@techsun.com>
2024-08-29 15:43:48 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+window.onload = function () {
+    //toc目录设置
+    var toc_img = document.querySelector('.toc');
+    var square = document.querySelector('ul');
+    var p_img = square.classList;
+    var img_button = toc_img.querySelectorAll('span');
+    var flage = '';
+    img_button[0].onclick = function (e) {
+        flage = img_button[0].className;
+        square.setAttribute('class', flage);
+    }
+    img_button[1].onclick = function (e) {
+        flage = img_button[1].className;
+        square.setAttribute('class', flage);
+    }
+    img_button[2].onclick = function (e) {
+        flage = img_button[2].className;
+        square.setAttribute('class', flage);
+    }
+    img_button[3].onclick = function (e) {
+        flage = img_button[3].className;
+        square.setAttribute('class', flage);
+    }
+    img_button[4].onclick = function (e) {
+        flage = img_button[4].className;
+        square.setAttribute('class', flage);
+    }
+    img_button[5].onclick = function (e) {
+        flage = img_button[5].className;
+        square.setAttribute('class', flage);
+    }
+}
+document.getElementById('nextTo').addEventListener('click', function () {
+    window.location.href = '/index2.html';
+})
+
